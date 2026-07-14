e1169a2870b68d4bd3b7e8e27632f7347ee982f3
Nithish <your_email@gmail.com>
2026-07-14 12:16:11 +0530

    Added animated stats

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FAQ
+
 const questions = document.querySelectorAll(".faq-question");
 
 questions.forEach(question => {
@@ -8,10 +10,42 @@ questions.forEach(question => {
 
         if(answer.style.display === "block"){
             answer.style.display = "none";
-        }else{
+        } else {
             answer.style.display = "block";
         }
 
     });
 
 });
+
+
+// ANIMATED STATS
+
+function animateValue(id, start, end, duration, suffix = "") {
+
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+
+    let stepTime = Math.abs(Math.floor(duration / range));
+
+    const obj = document.getElementById(id);
+
+    const timer = setInterval(() => {
+
+        current += increment;
+
+        obj.innerHTML = current + suffix;
+
+        if(current == end){
+            clearInterval(timer);
+        }
+
+    }, stepTime);
+
+}
+
+animateValue("businesses", 0, 8, 1000, "M+");
+animateValue("transactions", 0, 10, 1200, "T+");
+animateValue("countries", 0, 100, 1500, "+");
+animateValue("uptime", 0, 99, 1500, "%");
